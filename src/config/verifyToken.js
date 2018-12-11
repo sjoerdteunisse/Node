f8a273447db564debc6a3fdd51656dcc28f66ba9
Axon Robotics <sjoerdteunisse@gmail.com>
2018-12-11 09:25:25 +0100

    Added error handler on not being able to validate token

--- src/config/verifyToken.js
+++ src/config/verifyToken.js
@@ -1,29 +1,32 @@
 
 const jwtConfig = require('./jwtConfig');
 var jwtAsync = require('jsonwebtoken');
-const ApiError = require('../models/apierror.model');
+const apiError = require('../models/apierror.model');
 
 
 function verifyToken(req, res, next) {
     const token = req.headers['x-access-token'];
     
     if (!token)
-       return next(new ApiError('Authorization - No token provided.', '401'))
+       return next(new apiError('Authorization - No token provided.', '401'))
 
     console.log(token);
 
 
     jwtAsync.verify(token, jwtConfig.secret, function(err, decoded) {
    
-        if (decoded.id) {
-            req.userId = decoded.id;
-            next();
-        }
-        else {
-            //This shouldn't be a valid case. On Invalid Redirect?
-            next(new ApiError('Authorization - No token provided.', '401'));
+        if(err){
+            return next(new apiError('Authorization - Token invalid - try logging in again.', '401'))
         }
 
+        if (decoded.id) {
+            //Store id in request.
+            req.userId = decoded.id;
+            next();
+        } //Token not right
+        else {
+            next(new apiError('Authorization - No token provided.', '401'));
+        }
     });
 }
 
